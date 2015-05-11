9dbc355aba8c6fd59a4cf8490281511aa29f4c1f
nicolasnascimento <npn.adsl@gmail.com>
2015-05-11 16:58:56 -0300

    icons in store can have an image now

--- TapTheBacon/Product.swift
+++ TapTheBacon/Product.swift
@@ -15,15 +15,18 @@ class Product: NSObject, Printable {
     var multiplier: Int
     var autoClicks: Int
     
+    var imageName: String
+    
     
     override var description: String { return "name: " + self.name + " price: " + String(stringInterpolationSegment: price)}
     
-    init(name:String, price: Double, multiplier: Int, autoClicks: Int) {
+    init(name:String, price: Double, multiplier: Int, autoClicks: Int, imageName: String) {
         
         self.name = name
         self.price = price
         self.multiplier = multiplier
         self.autoClicks = autoClicks
+        self.imageName = imageName
         
         super.init()
     }
